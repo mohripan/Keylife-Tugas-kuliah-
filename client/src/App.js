138972b2a529250388de721dba98b5161e145c67
Rama Almer Felix <quenzvezda@gmail.com>
2022-05-04 00:33:03 +0700

    1:00:48

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,23 @@ import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatKeyLife, Header} from './containers';
 import {CTA, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatKeyLife />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
   )
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
-import './navbar.css';
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.png";
+import "./navbar.css";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wkeylife">What is Keylife</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default Navbar
+  return (
+    <div className="keylife__navbar">
+      <div className="keylife__navbar-links">
+        <div className="keylife__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="keylife__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="keylife__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="keylife__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="keylife__navbar-menu_container scale-up-center">
+            <div className="keylife__navbar-menu_container-links">
+              <Menu />
+              <div className="keylife__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
