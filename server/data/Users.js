a980271656fafa0193d1e3d5aff78eeff3f3a2ef
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2022-05-09 14:24:11 +0700

    fix hasSync

--- server/data/Users.js
+++ server/data/Users.js
@@ -4,13 +4,13 @@ const users = [
     {
         username: "Admin",
         email: "admin@admin.com",
-        password: bcrypt.hasSync("123456", 10),
+        password: bcrypt.hashSync("123456", 10),
         isAdmin: true
     },
     {
         username: "Mohripan",
         email: "mohripan@gmail.com",
-        password: bcrypt.hasSync("Mohripan123", 10)
+        password: bcrypt.hashSync("Mohripan123", 10)
     }
 ];
 
